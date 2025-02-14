8d9f60cfcc0aaa84cf58dededfe28fab4af5e05f
SamMarlborough24 <132004040+SamMarlborough24@users.noreply.github.com>
2025-02-13 16:44:13 -0800

    Done with comm101

--- comm101.R
+++ comm101.R
@@ -20,19 +20,24 @@ ggsave('comm101p3.png')
 # P4 Use ggplot to visualize the distribution of sea surface salinity by ocean
 # basin. Save your figure as “comm101p4.png”.
 
-# basin_colors <- ('#---', '#---', '#---', '#---', '#---',)
-ggplot(woa_sal, aes(basin, salinity, color = basin))+
+ggplot(woa_sal, aes(ocean, salinity))+
   geom_boxplot() +
-  theme_bw() +
-  scale_color_manual (values = basin_colors)
+  theme_bw()
 ggsave('comm101p4.png')
 
-# P5 Interpret your figures from P3 and P4. What patterns do you notice?
+# P5 Interpret your figures from P3 and P4. What patterns do you notice? The
+# histogram shows that the majority of salinity readings fell around 35
+# (presumably) ppt, consistent with the known global average
 
 # P6 Critique your figures from P3 and P4. What changes would highlight the
 # patterns you interpreted in P5? You don’t need to write code for these
 # changes, just describe them verbally.
 
+# Perhaps have the y-axis show just the 25 ppt - 45 ppt range in the boxplot.
+# This would cut off some of the lower range valuges in the Mediteranean region,
+# but would emphasize the differences between the ocean basins. Additionally,
+# this change makes sense because ocean salinity is known to be around 35 ppt on
+# average.
 
 # Relationships between continuous variables ------------------------------
 
@@ -43,19 +48,18 @@ ggplot(woa_sal, aes(latitude, salinity))+
 # P8 Edit your figure from P7 to improve its interpretability in at least one of
 # the following categories: visually differentiating the oceans, appropriateness
 # of the labels, or use of negative space. Save this figure as “comm101p8.png”.
-ggplot(woa_sal, aes(latitude, salinity))+
+basin_colors <- c('#005ff8','#6afbcf', '#00d5f3', '#359db3')
+
+ggplot(woa_sal, aes(latitude, salinity, color = ocean, shape = ocean)) +
   geom_point() +
-  labs(x = "Latitude", y = "Salinity") +
-  theme_bw()
+  labs(x = "Latitude (°)", y = "Salinity (ppt)",
+       color = "Ocean Basin", shape = "Ocean Basin") +
+  scale_shape_manual (values = 15:18 ) +
+  scale_color_manual (values = basin_colors) +
+  theme_bw() +
+  theme(legend.position = "inside",
+      legend.position.inside = c(0.01, 0.01),
+      legend.justification = c(0, 0))
 ggsave('comm101p8.png')
 
 
-# Code for when I have ocean basin data
-
-# ggplot(woa_sal, aes(latitude, salinity, color = basin, shape = basin))+
-# labs(x = "Latitude", y = "Salinity", color = "Ocean Basin")
-# scale_color_manual (values = basin_colors)
-# scale_shape_manual (values = 15:17 )
-# theme(legend.position = "inside",
-#  legend.position.inside = c(0.01, 0.01),
-#  legend.justification = c(0, 0))
